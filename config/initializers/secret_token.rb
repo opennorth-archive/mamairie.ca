0f1059665f5012c4a4fbba1eed5760d190cf0cbe
James McKinney <james@slashpoundbang.com>
2011-11-17 12:10:22 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaMairie::Application.config.secret_token = 'b340672bbf70ab68e0ee9171bad318adb353fd7e9023750188449aab9074e99f6f6e204963f713440d853146be33e81dfc4ce79a5b42264ee2d190fc0315effc'
