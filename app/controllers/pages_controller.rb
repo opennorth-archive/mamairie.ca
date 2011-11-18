751d1386b1470224ed8abf639be64b342de073a9
James McKinney <james@slashpoundbang.com>
2011-11-18 18:45:19 -0500

    add pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def index
+    @boroughs = Borough.fields(:name, :slug).all
+    @people = Person.all
+    # @todo activity feed
+  end
+
+  def search
+    if params[:postal_code].blank?
+      flash.alert = "Vous n'avez pas indiqué un code postal."
+    else
+      begin
+        redirect_to Borough.find_by_postal_code(params[:postal_code].gsub(/[^A-Za-z0-9]/, '').upcase)
+      rescue MongoMapper::DocumentNotFound
+        flash.alert = "Ce code postal n'est pas recensé dans notre système. Assurez-vous d'avoir entré un code postal de la Ville de Montréal."
+      rescue Timeout::Error
+        flash.alert = "Le site de la Ville de Montréal ne répond pas. S'il vous plaît réessayer."
+      end
+    end
+  end
+
+  def about
+    
+  end
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+end
