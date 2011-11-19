0b2116b1bf5c4a23407f4ec83ca2c4896778b8b4
James McKinney <james@slashpoundbang.com>
2011-11-19 15:54:29 -0500

    postal code search fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,15 +7,19 @@ class PagesController < ApplicationController
   end
 
   def search
-    if params[:postal_code].blank?
+    if params[:q].blank?
       flash.alert = "Vous n'avez pas indiqué un code postal."
+      redirect_to root_path
     else
       begin
-        redirect_to Borough.find_by_postal_code(params[:postal_code].gsub(/[^A-Za-z0-9]/, '').upcase)
+        redirect_to Borough.find_by_postal_code(params[:q].gsub(/[^A-Za-z0-9]/, '').upcase)
       rescue MongoMapper::DocumentNotFound
+        # @todo add postal code to error message
         flash.alert = "Ce code postal n'est pas recensé dans notre système. Assurez-vous d'avoir entré un code postal de la Ville de Montréal."
+        redirect_to root_path
       rescue Timeout::Error
         flash.alert = "Le site de la Ville de Montréal ne répond pas. S'il vous plaît réessayer."
+        redirect_to root_path
       end
     end
   end
