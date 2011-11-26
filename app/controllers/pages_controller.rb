582656a457c82ad53854cabc3cdac6162c1236b3
James McKinney <james@slashpoundbang.com>
2011-11-26 13:22:37 -0500

    track search errors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,9 +21,11 @@ class PagesController < ApplicationController
         borough = Borough.find_by_postal_code(params[:q].gsub(/[^A-Za-z0-9]/, '').upcase)
         redirect_to borough_path(id: borough.slug)
       rescue MongoMapper::DocumentNotFound
+        Query.find_or_create_by_query(params[:q]).update_attribute(found: false)
         flash.alert = "Le code postal <strong>#{params[:q]}</strong> n'est pas recensé dans notre système. Assurez-vous d'avoir entré un code postal de la Ville de Montréal.".html_safe
         redirect_to root_path
       rescue Timeout::Error, RestClient::GatewayTimeout, Errno::EHOSTUNREACH
+        Query.find_or_create_by_query('ERROR').increment(count: 1)
         flash.alert = "Le site de la Ville de Montréal ne répond pas. S'il vous plaît réessayer."
         redirect_to root_path
       end
--- app/models/query.rb
+++ app/models/query.rb
@@ -2,6 +2,7 @@ class Query
   include MongoMapper::Document
 
   key :query, String, required: true
+  key :found, Boolean, default: true
   key :count, Integer
 
   ensure_index :query
