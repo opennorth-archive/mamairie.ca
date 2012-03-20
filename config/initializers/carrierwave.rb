8d6ddc8de3c59ccc58b1ca3ebc26d08b8ec22668
James McKinney <james@slashpoundbang.com>
2012-03-20 16:54:31 -0400

    only use s3 in production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,16 @@
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
   config.cache_dir = 'uploads'
-  config.fog_credentials = {
-    :provider => 'AWS',
-    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-  }
-  config.fog_directory = 'public.slashpoundbang.com'
-  config.fog_host = '//public.slashpoundbang.com'
-  config.storage = :fog
+  if Rails.env.production?
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    }
+    config.fog_directory = 'public.slashpoundbang.com'
+    config.fog_host = '//public.slashpoundbang.com'
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
 end
