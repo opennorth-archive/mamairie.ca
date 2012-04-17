2062000b3e6fe45f68215614b03d6d95cc96ab0b
James McKinney <james@slashpoundbang.com>
2012-04-17 15:50:53 -0400

    improve readability of email validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -3,6 +3,8 @@ class EmailValidator < ActiveModel::EachValidator
   # Domain must be present and have two or more parts.
   def validate_each(record, attribute, value)
     address = Mail::Address.new value
-    record.errors[attribute] << (options[:message] || 'is invalid') unless (address.address == value && address.domain && address.__send__(:tree).domain.dot_atom_text.elements.size > 1 rescue false)
+    unless (address.address == value && address.domain && address.__send__(:tree).domain.dot_atom_text.elements.size > 1 rescue false)
+      record.errors[attribute] << (options[:message] || 'is invalid')
+    end
   end
 end
