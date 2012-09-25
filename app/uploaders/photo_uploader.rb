f02ec0d2f29efc7a84893a7cdf912e77df1ec27f
James McKinney <james@slashpoundbang.com>
2012-09-25 00:25:37 -0400

    fix carrierwave path

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,10 +1,16 @@
-# encoding: utf-8
+# coding: utf-8
+require 'carrierwave/processing/mime_types'
 
 class PhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MimeTypes
 
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+
+  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   # storage :fog
@@ -12,11 +18,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/mamairie.ca/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   def default_url
@@ -39,6 +48,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :scale => [50, 50]
   # end
+
   version :large do
     process :resize_to_fill => [90, 120]
   end
@@ -48,9 +58,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
   def extension_white_list
     %w(jpg jpeg gif png)
   end
