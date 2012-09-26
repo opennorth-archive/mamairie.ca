59ccce8f4d492246fc41e487a35a00acda811b9b
James McKinney <james@slashpoundbang.com>
2012-09-26 14:55:48 -0400

    move address, source, party, district to engine

--- spec/models/address_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Address do
-  [:name, :address].each do |attribute|
-    should validate_presence_of :attribute
-  end
-end
--- spec/models/party_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Party do
-  [:name, :slug].each do |attribute|
-    should validate_presence_of attribute
-  end
-end
