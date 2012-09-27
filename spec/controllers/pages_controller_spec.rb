7a5a81be4c36e41074426f6f3b7f6a3a78584493
James McKinney <james@slashpoundbang.com>
2012-09-27 12:39:19 -0400

    remove specs that have moved to engine

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
+  describe 'GET index' do
+    it 'should be successful' do
+      get :index
+      response.code.should == 200
+      response.should render_template('index')
+    end
 
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
+    it 'should render the Atom feed' do
+      get :index, format: 'atom'
+      response.code.should == 200
+      response.should render_template('index')
     end
   end
 
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
+  describe 'GET search' do
+    pending
   end
-
 end
