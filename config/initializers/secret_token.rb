f66c06d50c57e6849041f04d4ac444161fb9226c
James McKinney <james@slashpoundbang.com>
2013-12-19 16:50:04 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyCityHall::Application.config.secret_key_base = '01539c6e8962cd6ab1fd3cf3b63e38afd0a3d91d0312413e2d508e2c4ae303cd886b8a7f2eeef17f3a25ad67e2b7590ac71ec03929fb67c1ee60975236430632'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyCityHall::Application.config.session_store :cookie_store, key: '_mycityhall_session'
